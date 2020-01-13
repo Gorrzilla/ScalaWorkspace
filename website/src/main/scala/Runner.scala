cbff5924c24a7e1e449dc1ccb936fc1f1d9c3f9b
Andrew <andrew.gorrie13@gmail.com>
2020-01-13 20:11:11 +0000

    Now prints Lat/Long to page instead of console

--- website/src/main/scala/Runner.scala
+++ website/src/main/scala/Runner.scala
@@ -8,16 +8,26 @@ object Runner {
 
   def main(args: Array[String]): Unit = {
 
+
+
+
+
+
      val opts = js.Object().asInstanceOf[PositionOptions]
      opts.timeout = 50000
      opts.enableHighAccuracy = true
 
     window.navigator.geolocation.getCurrentPosition(
       {pos: dom.Position => 
-      val latitude = pos.coords.latitude
       val longitude = pos.coords.longitude
-      println("Latitude is: " + latitude)
-      println("Longitude is " + longitude)}, 
+      val parNode = document.createElement("p")
+      val latitudeNode = document.createTextNode("Your Latitude is " + pos.coords.latitude)
+      val longitudeNode = document.createTextNode("Your Longitude is " + pos.coords.longitude)
+      val br = document.createElement("br")
+      parNode.appendChild(latitudeNode)
+      parNode.appendChild(br)
+      parNode.appendChild(longitudeNode)
+      document.body.appendChild(parNode)}, 
       {err: PositionError =>
         println(err.code)
         println(err.message)}
