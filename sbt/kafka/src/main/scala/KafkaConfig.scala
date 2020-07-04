06ed4872b6cb9f49c1b0bd0a804bb51a14fdfeb0
Andrew Gorrie <agorrie13@gmail.com>
2020-07-04 11:39:19 +0100

    Added logic to validate user provided IP Address

--- sbt/kafka/src/main/scala/KafkaConfig.scala
+++ sbt/kafka/src/main/scala/KafkaConfig.scala
@@ -1,22 +1,45 @@
+import java.util.Arrays
+
 class KafkaConfig() {
 
   private var _brokerPort: String = ""
   private var _topic: String = ""
 
   //Getter
-  def brokerPort = _brokerPort
+  def brokerPort: String = _brokerPort
 
   //Getter
-  def topic = _topic
+  def topic: String = _topic
 
   //Setter
-  def brokerPort_= (providedPort: String) = {
-    _brokerPort = providedPort
+  def brokerPort_= (providedPort: String): Unit = {
+    val isValid = validateAddress(providedPort)
+      if (isValid) _brokerPort = providedPort
   }
 
   //Setter
-  def topic_=(providedTopics: String)={
+  def topic_=(providedTopics: String): Unit ={
     _topic = providedTopics
+  }
+
+  def validateAddress(providedIp: String) : Boolean = {
+
+    var groups = providedIp.split("\\.")
+    if (groups.length !=4) {
+      return false
+    }
+    try{
+      for (num <- groups){
+        val numericRep = num.toInt
+          if ((numericRep < 0) || (numericRep > 255)){
+            return false
+          }
+      }
+      true
+    }
+    catch{
+      case e : NumberFormatException => false
+    }
   }
 
 }
