c4493748e1de560e9325972a500b7858a1ce1e00
Andrew Gorrie <agorrie13@gmail.com>
2020-07-04 08:45:07 +0100

    Initial tests for KafkaConfig

--- sbt/kafka/src/test/scala/ProducerSpecs.scala
+++ sbt/kafka/src/test/scala/ProducerSpecs.scala
@@ -1,3 +1,22 @@
-class ProducerSpecs {
+import org.scalatest.FunSpec
+import org.scalatest.GivenWhenThen
 
+class ProducerSpecs extends FunSpec with GivenWhenThen{
+  {
+    {
+      describe("A Kafka Configuration File") {
+
+        it("Should have null defaults") {
+
+          Given("No additional parameters are provided")
+          val cfg = new KafkaConfig()
+
+          When("It is originally created")
+          assert(cfg.topic.isEmpty)
+          assert(cfg.brokerPort.isEmpty)
+        }
+      }
+
+    }
+  }
 }
