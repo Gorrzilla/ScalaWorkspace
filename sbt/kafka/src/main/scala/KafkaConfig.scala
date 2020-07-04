61d0a0880bf73f427a563c12c070faac1d9f24f4
Andrew Gorrie <agorrie13@gmail.com>
2020-07-04 08:43:47 +0100

    Added initial implementation

--- sbt/kafka/src/main/scala/KafkaConfig.scala
+++ sbt/kafka/src/main/scala/KafkaConfig.scala
@@ -1,3 +1,22 @@
-class ConsumerConfig {
+class KafkaConfig() {
+
+  private var _brokerPort: String = ""
+  private var _topic: String = ""
+
+  //Getter
+  def brokerPort = _brokerPort
+
+  //Getter
+  def topic = _topic
+
+  //Setter
+  def brokerPort_= (providedPort: String) = {
+    _brokerPort = providedPort
+  }
+
+  //Setter
+  def topic_=(providedTopics: String)={
+    _topic = providedTopics
+  }
 
 }
