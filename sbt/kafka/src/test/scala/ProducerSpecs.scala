c75414f0a8574858cbb789865d32868c86a89856
Andrew Gorrie <agorrie13@gmail.com>
2020-07-04 09:02:34 +0100

    Added further tests

--- sbt/kafka/src/test/scala/ProducerSpecs.scala
+++ sbt/kafka/src/test/scala/ProducerSpecs.scala
@@ -15,8 +15,47 @@ class ProducerSpecs extends FunSpec with GivenWhenThen{
           assert(cfg.topic.isEmpty)
           assert(cfg.brokerPort.isEmpty)
         }
-      }
+
+        it("Should allow users to specify a topic"){
+
+          Given("The user provides the topic")
+          val topic = "HelloWorld"
+
+          When("After it has been created")
+          val cfg = new KafkaConfig()
+          cfg.topic = topic
+          assert(cfg.topic == topic)
+
+        }
+
+        it("Should allow users to specify a broker port"){
+
+          Given("The user provides the port")
+          val port = "192.168.1.1"
+
+          When("After it has been created")
+          val cfg = new KafkaConfig()
+          cfg.brokerPort = port
+          assert(cfg.brokerPort == port)
+
+        }
+
+        it("Should only allow valid ip addresses to be set"){
+
+          Given("The user provides the port")
+          val port = "172.8.7.28"
+
+          When("After it has been created")
+          val cfg = new KafkaConfig()
+          cfg.brokerPort = port
+
+          Then("It should reject the provided address")
+          assert(cfg.brokerPort.isEmpty)
+
+        }      }
 
     }
+
+
   }
 }
