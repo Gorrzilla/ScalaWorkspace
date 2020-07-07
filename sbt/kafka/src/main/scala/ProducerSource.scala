1aad17429a949b8023ed842566cc1e8f3dee8556
Andrew Gorrie <agorrie13@gmail.com>
2020-07-07 18:24:18 +0100

    Initial attempt at factory method instantiation

--- sbt/kafka/src/main/scala/ProducerSource.scala
+++ sbt/kafka/src/main/scala/ProducerSource.scala
@@ -1,5 +1,47 @@
+import java.io.File
+
 abstract class ProducerSource {
 
   def process(): Unit
 
+  object ProducerSource {
+
+  private class Console extends ProducerSource {
+    override def process{}
+  }
+
+    private class TextBased extends ProducerSource {
+      override def process{}
+    }
+
+    private class BinaryBased extends ProducerSource {
+      override def process{}
+    }
+
+    //Factory method
+    def apply(arg: String): ProducerSource = {
+      val isFile  = new File(arg).exists()
+      val isValidIP = KafkaConfig.validateAddress(arg)
+      if (isFile){
+
+      }
+      else if (isValidIP){
+
+      }
+        {
+
+        }
+      // If is string then select the appropriate class based on extension
+
+      // If not string then check if IP provided
+
+      // Otherwise exit!
+
+
+    }
+
+
+
+
+  }
 }
