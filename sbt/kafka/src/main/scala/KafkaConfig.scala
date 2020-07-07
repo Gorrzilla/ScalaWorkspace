955b69dc6ab6aed64c481f190552b80b46dbf681
Andrew Gorrie <agorrie13@gmail.com>
2020-07-07 17:30:00 +0100

    Added Creation of configuration object methods to KafkaConfig Class.

--- sbt/kafka/src/main/scala/KafkaConfig.scala
+++ sbt/kafka/src/main/scala/KafkaConfig.scala
@@ -1,15 +1,25 @@
-import java.util.Arrays
+import java.util.Properties
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 
 class KafkaConfig() {
 
   private var _brokerPort: String = ""
   private var _topic: String = ""
+  private var _groupID: Int = null
+  private var producerConfiguration: Properties = _
+  private var consumerConfiguration: Properties = _
 
   //Getter
   def brokerPort: String = _brokerPort
 
   //Getter
   def topic: String = _topic
+
+  //Getter
+  def groupID: Int = _groupID
 
   //Setter
   def brokerPort_= (providedPort: String): Unit = {
@@ -20,6 +30,11 @@ class KafkaConfig() {
   //Setter
   def topic_=(providedTopics: String): Unit ={
     _topic = providedTopics
+  }
+
+  //Setter
+  def groupID_=(providedID: Int): Unit ={
+    _groupID = providedID
   }
 
   def validateAddress(providedIp: String) : Boolean = {
@@ -41,5 +56,24 @@ class KafkaConfig() {
       case e : NumberFormatException => false
     }
   }
+
+  private def createProducerConfiguration(brokers: String): Properties = {
+    val props = new Properties()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getCanonicalName)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getCanonicalName)
+    props
+  }
+
+  private def createConsumerConfiguration(brokers: String, groupID: Int): Properties = {
+      val props = new Properties()
+      props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+      props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getCanonicalName)
+      props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getCanonicalName)
+      props.put(ConsumerConfig.GROUP_ID_CONFIG, groupID)
+      props
+  }
+
+
 
 }
