19f8c34852a56784da48b7c886825313069eb88e
Andrew Gorrie <agorrie13@gmail.com>
2020-07-10 14:46:48 +0100

    Returns correct object depending on provided argument

--- sbt/kafka/src/main/scala/ProducerSource.scala
+++ sbt/kafka/src/main/scala/ProducerSource.scala
@@ -1,13 +1,19 @@
 import java.io.File
+import scala.io.StdIn
+import java.nio.file.Paths
 
 abstract class ProducerSource {
-
   def process(): Unit
+}
 
   object ProducerSource {
 
-  private class Console extends ProducerSource {
-    override def process{}
+  private class CommandLineConsole extends ProducerSource {
+    override def process: Unit ={
+      println("Enter message (type exit to quit)")
+      var message = StdIn.readLine()
+
+    }
   }
 
     private class TextBased extends ProducerSource {
@@ -18,30 +24,31 @@ abstract class ProducerSource {
       override def process{}
     }
 
+    private class PacketBased extends ProducerSource {
+      override def process {}
+    }
+
     //Factory method
     def apply(arg: String): ProducerSource = {
       val isFile  = new File(arg).exists()
       val isValidIP = KafkaConfig.validateAddress(arg)
+
       if (isFile){
+        // Get the file extension
+        val extension = Paths.get(arg).getFileName.toString.split("\\.").last
+
+        extension match {
+          case "txt" => new TextBased
+          case "bin" => new BinaryBased
+        }
 
       }
       else if (isValidIP){
-
+        new PacketBased
       }
+      else
         {
-
+          new CommandLineConsole
         }
-      // If is string then select the appropriate class based on extension
-
-      // If not string then check if IP provided
-
-      // Otherwise exit!
-
-
     }
-
-
-
-
-  }
 }
