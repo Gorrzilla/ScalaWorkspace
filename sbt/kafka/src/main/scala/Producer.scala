5ac59e0739ff1cf42643da872e12b461e4489295
Andrew Gorrie <agorrie13@gmail.com>
2020-07-10 14:47:55 +0100

    Now instantiates Producer Source

--- sbt/kafka/src/main/scala/Producer.scala
+++ sbt/kafka/src/main/scala/Producer.scala
@@ -1,38 +1,32 @@
 import java.util.Properties
 
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
-import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
-class Producer (topic: String, brokers: String){
+class Producer (cfg : KafkaConfig){
+  val kafkaProducerConfig: Properties = cfg.createProducerConfiguration(cfg.topic)
 
-  val producer = new KafkaProducer[String, String](configuration)
+  val producer = new KafkaProducer[String, String](kafkaProducerConfig)
 
-  private def configuration: Properties = {
-    val props = new Properties()
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getCanonicalName)
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getCanonicalName)
-    props
-  }
-
-  def sendMessages(): Unit = {
-    println("Enter message (type exit to quit)")
-    //var message = StdIn.readLine()
-
-    //while (! message.equals("exit")) {
-      //val record = new ProducerRecord[String, String](topic, "1", message)
-      //producer.send(record)
-      //message = StdIn.readLine()
-    //}
-
-    //producer.close()
+  def sendMessages(producerSource: ProducerSource): Unit = {
+    producerSource.process()
   }
 
 }
 
 object Producer extends App {
 
-  val producer = new Producer(brokers = args(0), topic = args(1))
-  producer.sendMessages()
+  val kafkaConfig = new KafkaConfig()
+  kafkaConfig.brokerPort = args(0)
+  kafkaConfig.topic = args(1)
 
+  if (kafkaConfig.brokerPort.isEmpty){
+    System.exit(-1)
+  }
+
+  val prodSource = ProducerSource.apply(args(2))
+  val producer = new Producer(kafkaConfig)
+
+  producer.sendMessages(prodSource)
 }
+
+//TODO add logging framework
